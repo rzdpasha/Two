9747acc664dcacd102b385d9f40055efa9611c8d
Zverev P.A <rzdpasha@yandex.ru>
2023-05-28 19:39:54 +0300

    Add List.py + mod README.md

---
+++ List.py
@@ -0,0 +1,20 @@
+#print(list(range(0, 30, 5)))
+#print(list(range(0)))
+#r = range(0, 20, 2)
+#print(r)
+#range(0, 20, 2)
+#11 in r
+
+#10 in r
+
+#r.index(10)
+
+#r[5]
+
+#r[:5]
+#range(0, 10, 2)
+#r[-1]
+#print('MiscTests'.removesuffix('Tests'))
+#print('QwertyTest'.removeprefix('Qwerty'))
+#print('432'.zfill(7))
+print(b'Py' in b'Python')
